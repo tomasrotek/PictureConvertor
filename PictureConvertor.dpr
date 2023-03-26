e5e4601f9f6e0b30a7dbf5cd441656014bef3ce7
rotektomas <rotektomas55@gmail.com>
2023-03-26 17:03:42 +0200

    Added references

--- PictureConvertor.dpr
+++ PictureConvertor.dpr
@@ -4,7 +4,9 @@ uses
   Vcl.Forms,
   MainForm in 'MainForm.pas' {Main},
   PictureConvertor.Form.Methods in 'PictureConvertor.Form.Methods.pas',
-  PictureConvertor.Picture.Methods in 'PictureConvertor.Picture.Methods.pas';
+  PictureConvertor.Picture.Methods in 'PictureConvertor.Picture.Methods.pas',
+  PictureConvertor.Picture.Types in 'PictureConvertor.Picture.Types.pas',
+  PictureConvertor.Picture.Thread in 'PictureConvertor.Picture.Thread.pas';
 
 {$R *.res}
 
